8f14d87178c0e53c74a42161796ba17b9dcb49bc
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-06 13:15:31 +0100

    Added functionality to create new user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,27 @@
-module.exports.signup_get = (req,res) => {
-    res.render('signup')
-}
-module.exports.login_get = (req,res) => {
-    res.render('login')
-}
-module.exports.signup_post = (req,res) => {
-    res.render('new signup')
-}
-module.exports.login_post = (req,res) => {
-    res.render('user login')
-}
+const User = require("../models/User");
+
+module.exports.signup_get = (req, res) => {
+  res.render("signup");
+};
+
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("error, user not created");
+  }
+};
+
+module.exports.login_post = async (req, res) => {
+  const { name, email } = req.body;
+  console.log(name, email);
+  res.send("user login");
+};
