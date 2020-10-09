f1fd111a2accbfafaddcff1f68f40e96b921f1cb
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-09 11:12:54 +0100

    User password encryption with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,17 +17,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// fire a function after doc saved to db
-userSchema.post('save',function(doc,next){
-  console.log('new user',doc);
-
-  // always remember to call next!!
-  next();
-})
 
 // fire a function before doc saved to db
-userSchema.pre('save',function(next){
-  console.log('user aboout to be created',this);
+userSchema.pre('save', async function(next){
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password,salt);
+  // Always remember to call next
   next();
 })
 
