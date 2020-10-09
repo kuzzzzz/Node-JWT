5f610721a5eb6739b74028484f01efa82b43b9f1
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-09 13:11:52 +0100

    Cookie Review lesson

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -18,12 +20,31 @@ mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
-app.use(authRoutes)
+app.use(authRoutes);
+
+// cookies
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("set-cookies", "newUser=true");
+
+  res.cookie("newUser", false);
+  res.cookie("isEmployer", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+
+  res.send("you got the cookie!");
+});
+
+app.get('/read-cookies',(req,res)=>{
+  const cookies = req.cookies;
+  console.log(cookies)
+  res.json(cookies);
+})
