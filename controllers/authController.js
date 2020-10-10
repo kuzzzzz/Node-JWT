88bb0c37319f311947d2abad9bc1dd227b2d0a7b
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-10 22:42:28 +0100

    login functionality added

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,11 @@ module.exports.signup_post = async (req, res) => {
 };
 
 module.exports.login_post = async (req, res) => {
-  const { name, email } = req.body;
-  console.log(name, email);
-  res.send("user login");
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(400).json({});
+  }
 };
--- models/User.js
+++ models/User.js
@@ -1,30 +1,42 @@
 const mongoose = require("mongoose");
-const {isEmail} = require('validator');
-const bcrypt = require('bcrypt')
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-  required: [true,'Please enter an email'],
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
-    validate:[isEmail,'Please enter a valid email']
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: [true, 'Please enter an password'],
-    minlength: [6,'Minimum password length is 6 characters'],
+    required: [true, "Please enter an password"],
+    minlength: [6, "Minimum password length is 6 characters"],
   },
 });
 
-
 // fire a function before doc saved to db
-userSchema.pre('save', async function(next){
+userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password,salt);
+  this.password = await bcrypt.hash(this.password, salt);
   // Always remember to call next
   next();
-})
+});
+
+// static method to login user
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect password");
+  }
+  throw Error("Incorrect email");
+};
 
 const User = mongoose.model("user", userSchema);
 
