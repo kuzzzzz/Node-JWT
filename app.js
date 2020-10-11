e7fad3140b97a052c1b7d9ae0514960610472216
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-11 19:47:02 +0100

    Added middleware function to protect routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
+const {requireAuth} = require('./Middleware/authMiddleware')
 
 const app = express();
 
@@ -27,7 +28,9 @@ mongoose
 
 // routes
 app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", (req, res) => res.render("smoothies"));
+
+// added middleware function to protect routes
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(authRoutes);
 
 
