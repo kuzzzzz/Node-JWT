5d9800325060e6ddf554130194ba6bbb9c22152b
Atumah Daniel <atumahdaniel@gmail.com>
2020-10-11 21:00:48 +0100

    conditional rendering

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -23,7 +23,7 @@ const requireAuth = (req, res, next) => {
 
 // check current user
 const checkUser = (req, res, next) => {
-  const token = req.cookie.jwt;
+  const token = req.cookies.jwt;
 
   if (token) {
     jwt.verify(token, "net ninja secret", async (err, decodedToken) => {
